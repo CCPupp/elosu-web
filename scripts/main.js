05ecb94af0707af68e758fa2227afefe22762087
[Pupper] <[cjerskine@gmail.com]>
2019-02-14 16:49:20 -0500

    oops script actually works now

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $("#top").on('click', function() {
+$(document).ready(function () {
+    $("#top").on('click', function () {
         var tops = $("#tops").val();
         $.ajax({
             url: "/top",
@@ -8,12 +8,12 @@ $(document).ready(function() {
             data: {
                 tops: tops
             },
-            success: function(data) {
+            success: function (data) {
                 $("#response").html(data);
             },
         });
     });
-    $("#calc").on('click', function() {
+    $("#calc").on('click', function () {
         var P1 = $("#P1").val();
         var P2 = $("#P2").val();
         $.ajax({
@@ -24,33 +24,33 @@ $(document).ready(function() {
                 P1: P1,
                 P2: P2
             },
-            success: function(data) {
+            success: function (data) {
                 $("#response").html(data);
             },
         });
     });
-    $("#clear").on('click', function() {
+    $("#clear").on('click', function () {
         $.ajax({
             url: "https://pupper.moe/clear",
             method: "GET",
-            success: function(data) {
+            success: function (data) {
                 $("#response").html(data);
             },
         });
     });
-});
-// Collapseable Script
-var coll = document.getElementsByClassName("collapsible");
-var i;
+    // Collapseable
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
     }
-  });
-}
+});
